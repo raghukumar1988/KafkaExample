194a93e1f2186f5f0e87f79577b1959680df2654
Raghukumar Rajamani <rraghukumar.ece@gmail.com>
2020-10-21 12:20:29 +0530

    Initial commit

---
+++ src/main/java/com/demo/models/User.java
@@ -0,0 +1,15 @@
+package com.demo.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+ }
+}
